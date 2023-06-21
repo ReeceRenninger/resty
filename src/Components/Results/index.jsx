58969f026422b5fdae13e6f798b3da4920868d8f
ReeceRenninger <renningerreece@gmail.com>
2023-06-21 13:29:28 -0700

    converted App into function and started useState conversion

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Results.scss';
 
 function Results(props) {
-  
+  //TODO: add some type of new html element for loading message
     return (
       <section>
         <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
